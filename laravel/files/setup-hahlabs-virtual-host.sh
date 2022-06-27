4b7bef395a8a6d69a2750b0213c4127856cd292e
Hatem Abou Hassan <habouhassan@hahlabs.com>
2022-06-27 08:39:13 -0700

    enable XDEBUG

--- laravel/files/setup-hahlabs-virtual-host.sh
+++ laravel/files/setup-hahlabs-virtual-host.sh
@@ -4,7 +4,7 @@
 composer create-project laravel/laravel hahlabs-app
 chmod -R gou+rwx hahlabs-app/storage
 chown -R hahlabs:hahlabs hahlabs-app
-cp files/nginx-hahlabs-app /etc/nginx/sites-available/hahlabs-app 
+cp files/nginx-hahlabs-app-virtual-host /etc/nginx/sites-available/hahlabs-app 
 ln -s /etc/nginx/sites-available/hahlabs-app /etc/nginx/sites-enabled/hahlabs-app 
 ln -s /home/hahlabs/hahlabs-app /var/www/hahlabs-app
 cp files/nginx.index.html /var/www/hahlabs-app/public/index-test.html 
