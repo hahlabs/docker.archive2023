50ac3394b03360bb7e218233561a7073bae220ca
Hatem Abou Hassan <habouhassan@hahlabs.com>
2023-03-16 09:33:53 -0700

    Update user auth for hahlabs to use auth_socket_plugin

--- mysql/files/root-user.sql
+++ mysql/files/root-user.sql
@@ -1,5 +1,8 @@
+USE mysql;
 CREATE USER 'root'@'%' IDENTIFIED BY 'Root!123';
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
 CREATE USER 'hahlabs'@'%' IDENTIFIED BY 'Hahlabs!123';
 GRANT ALL PRIVILEGES ON *.* TO 'hahlabs'@'%' WITH GRANT OPTION;
+UPDATE user SET plugin='auth_socket' WHERE User='hahlabs';
 FLUSH PRIVILEGES;
+
