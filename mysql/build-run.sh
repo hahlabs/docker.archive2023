de42aa755137657dc3593b17bf0729ccf903372a
timyshark <tim.a.hass@outlook.com>
2023-12-31 10:52:18 +0000

    fixes to enable AWS SSM access and SSH startup for FARGATE

--- mysql/build-run.sh
+++ mysql/build-run.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 #usage ./buid-run.sh <image-tag>  ex/ alpha-0.1.12 or lts-3
-docker system prune -f
 docker rm -f $(docker ps -a -q)
+docker system prune -f
+
 ../scripts/docker-build.sh
 . .env
 echo "Docker container:" $CONTAINER_NAME $IMAGE_TAG
 # NOTE : do not map ports for WSL 
-docker run -ditp 3306:3306 -p 22:22 \
+docker run -dit \
      --name $CONTAINER_NAME $IMAGE_TAG
 
 docker exec -it `docker ps | sed -n /$CONTAINER_NAME/p  | sed -e 's/\(^[[:xdigit:]]\{12\}\).*$/\1/g'` /bin/bash
