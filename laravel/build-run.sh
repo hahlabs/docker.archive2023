0bab0c018b88f0787344c387971946d8405887a4
timyshark <tim.a.hass@outlook.com>
2023-12-31 22:41:13 +0000

    enhancing script

--- laravel/build-run.sh
+++ laravel/build-run.sh
@@ -12,3 +12,5 @@ docker run -dit \
      --name $CONTAINER_NAME $IMAGE_TAG "/bin/bash"
 
 docker exec -it `docker ps | sed -n /$CONTAINER_NAME/p  | sed -e 's/\(^[[:xdigit:]]\{12\}\).*$/\1/g'` /bin/bash
+#Same...
+#docker exec -it $CONTAINER_NAME /bin/bash
