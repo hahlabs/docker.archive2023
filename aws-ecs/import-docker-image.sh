e6e83d7deaefe748ee68cb60b9349a62392e5f53
Edudater <edudate@edudate.academy>
2024-01-01 17:32:58 -0800

    Use -q to pull and push

--- aws-ecs/import-docker-image.sh
+++ aws-ecs/import-docker-image.sh
@@ -2,9 +2,9 @@ docker logout
 docker rm -f $(docker ps -aq)
 docker image rm -f $(docker images -q)
 cat /projects/docker-hahlabs-access-key.txt| docker login --username hahlabs --password-stdin
-docker pull hahlabs/mysql:dev & \
-docker pull hahlabs/laravel:dev & \
-docker pull hahlabs/angular:dev  && fg
+docker pull -q hahlabs/mysql:dev & \
+docker pull -q hahlabs/laravel:dev & \
+docker pull -q hahlabs/angular:dev  && fg
 
 docker logout
 aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 479788010133.dkr.ecr.us-west-2.amazonaws.com
@@ -12,6 +12,6 @@ docker tag hahlabs/mysql:dev 479788010133.dkr.ecr.us-west-2.amazonaws.com/hahlab
 docker tag hahlabs/laravel:dev 479788010133.dkr.ecr.us-west-2.amazonaws.com/hahlabs/laravel:dev
 docker tag hahlabs/angular:dev 479788010133.dkr.ecr.us-west-2.amazonaws.com/hahlabs/angular:dev
 
-docker push 479788010133.dkr.ecr.us-west-2.amazonaws.com/hahlabs/mysql:dev & \
-docker push 479788010133.dkr.ecr.us-west-2.amazonaws.com/hahlabs/laravel:dev & \
-docker push 479788010133.dkr.ecr.us-west-2.amazonaws.com/hahlabs/angular:dev && fg
+docker push -q 479788010133.dkr.ecr.us-west-2.amazonaws.com/hahlabs/mysql:dev & \
+docker push -q 479788010133.dkr.ecr.us-west-2.amazonaws.com/hahlabs/laravel:dev & \
+docker push -q 479788010133.dkr.ecr.us-west-2.amazonaws.com/hahlabs/angular:dev && fg
