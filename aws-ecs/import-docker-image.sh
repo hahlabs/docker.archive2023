c746fa10772077f90f7428f0d7239ee9b79792f2
Edudater <edudate@edudate.academy>
2024-01-01 18:09:53 -0800

    Restructuring

--- aws-ecs/import-docker-image.sh
+++ aws-ecs/import-docker-image.sh
@@ -7,7 +7,7 @@ docker pull -q hahlabs/laravel:dev & \
 docker pull -q hahlabs/angular:dev  && fg
 
 docker logout
-aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 479788010133.dkr.ecr.us-west-2.amazonaws.com
+
 docker tag hahlabs/mysql:dev 479788010133.dkr.ecr.us-west-2.amazonaws.com/hahlabs/mysql:dev
 docker tag hahlabs/laravel:dev 479788010133.dkr.ecr.us-west-2.amazonaws.com/hahlabs/laravel:dev
 docker tag hahlabs/angular:dev 479788010133.dkr.ecr.us-west-2.amazonaws.com/hahlabs/angular:dev
