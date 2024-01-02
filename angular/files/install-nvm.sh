8a14dc5ed14dfe575bad646b57087ebfe0867b34
Edudater <edudate@edudate.academy>
2024-01-01 23:41:20 -0800

    Updates to ssh

--- angular/files/install-nvm.sh
+++ angular/files/install-nvm.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
-export NVM_DIR="$HOME/.nvm"  
+cat <<EOF >>~/.bashrc
+#!/bin/sh
+# Created on $(date)
+export NVM_DIR="$HOME/.nvm" 
+[ -s "\$NVM_DIR/nvm.sh" ] && \. "\$NVM_DIR/nvm.sh" 
+[ -s "\$NVM_DIR/bash_completion" ] && \. "\$NVM_DIR/bash_completion" 
+
+EOF
+
+export NVM_DIR="$HOME/.nvm" 
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" 
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" 
 nvm install 20.10.0
+
--- laravel/build-run.sh
+++ laravel/build-run.sh
@@ -4,11 +4,11 @@
 ../scripts/docker-build.sh
 . .env
 echo "Docker container:" $CONTAINER_NAME $IMAGE_TAG
-# docker run -dit \
-#      -w /home/hahlabs  \
-#      --mount type=bind,source="$(pwd)",target=/app  \
-#      --name $CONTAINER_NAME $IMAGE_TAG "/bin/bash"
+docker run -dit \
+     -w /home/hahlabs  \
+     --mount type=bind,source="$(pwd)",target=/app  \
+     --name $CONTAINER_NAME $IMAGE_TAG "/bin/bash"
 
 # docker exec -it `docker ps | sed -n /$CONTAINER_NAME/p  | sed -e 's/\(^[[:xdigit:]]\{12\}\).*$/\1/g'` /bin/bash
-#Same...
-#docker exec -it $CONTAINER_NAME /bin/bash
+
+docker exec -it $CONTAINER_NAME /bin/bash
