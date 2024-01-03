ce99ba3b79da02330b9c5fcd207b4db7ee5f3fc7
Edudater <edudate@edudate.academy>
2024-01-03 08:43:43 -0800

    Refactor build to nohup and logging

--- scripts/build-all.sh
+++ scripts/build-all.sh
@@ -2,10 +2,11 @@
 docker rm -f $(docker ps -aq)
 docker rmi -f $(docker images -q)
 docker system prune -af
-
-cd ../ubuntu && ./build-run
-cd ../mysql && ./build-run & 
-cd ../laravel && ./build-run & 
-cd ../angular && ./build-run &
+echo "HAHLABS docker builds errors $(date)" >> ../scripts/hahlabs-build-run.err
+echo "HAHLABS docker builds starts $(date)" >> ../scripts/hahlabs-build-run.log
+cd ../ubuntu && ./build-run > ../scripts/hahlabs-build-run.log 2> ../scripts/hahlabs-build-run.err < /dev/null
+cd ../mysql && nohup ./build-run > ../scripts/hahlabs-build-run.log 2> ../scripts/hahlabs-build-run.err < /dev/null & 
+cd ../laravel && nohup ./build-run > ../scripts/hahlabs-build-run.log 2> ../scripts/hahlabs-build-run.err < /dev/null & 
+cd ../angular && nohup ./build-run > ../scripts/hahlabs-build-run.log 2> ../scripts/hahlabs-build-run.err < /dev/null &
 
 jobs
