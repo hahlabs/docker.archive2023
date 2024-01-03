f018aefdae567c0bc69cce8e6a8cd45e951d6c04
Edudater <edudate@edudate.academy>
2024-01-03 10:17:28 -0800

    Fixes

--- scripts/build-all.sh
+++ scripts/build-all.sh
@@ -4,6 +4,7 @@ docker rmi -f $(docker images -q)
 docker system prune -af
 echo "HAHLABS docker builds errors $(date)" > ../scripts/hahlabs-build-run.err
 echo "HAHLABS docker builds starts $(date)" > ../scripts/hahlabs-build-run.log
+echo "Build Ubuntu base image...Please wait"
 cd ../ubuntu && ./build-run >> ../scripts/hahlabs-build-run.log 2>> ../scripts/hahlabs-build-run.err < /dev/null
 cd ../mysql && nohup ./build-run >> ../scripts/hahlabs-build-run.log 2>> ../scripts/hahlabs-build-run.err < /dev/null & 
 cd ../laravel && nohup ./build-run >> ../scripts/hahlabs-build-run.log 2>> ../scripts/hahlabs-build-run.err < /dev/null & 
