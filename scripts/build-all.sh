96c400c0238dcb397cf97285027814f7bf764339
Edudater <edudate@edudate.academy>
2024-01-04 09:18:39 -0800

    Change the location of build logs to the folder logs

--- scripts/build-all.sh
+++ scripts/build-all.sh
@@ -2,12 +2,12 @@
 docker rm -f $(docker ps -aq)
 docker rmi -f $(docker images -q)
 docker system prune -af
-echo "HAHLABS docker builds errors $(date)" > ../scripts/hahlabs-build-run.err
-echo "HAHLABS docker builds starts $(date)" > ../scripts/hahlabs-build-run.log
+echo "HAHLABS docker builds errors $(date)" > ../scripts/logs/hahlabs-build-run.err
+echo "HAHLABS docker builds starts $(date)" > ../scripts/logs/hahlabs-build-run.log
 echo "Build Ubuntu base image...Please wait"
-cd ../ubuntu && ./build-run >> ../scripts/hahlabs-build-run.log 2>> ../scripts/hahlabs-build-run.err < /dev/null
-cd ../mysql && nohup ./build-run >> ../scripts/hahlabs-build-run.log 2>> ../scripts/hahlabs-build-run.err < /dev/null & 
-cd ../laravel && nohup ./build-run >> ../scripts/hahlabs-build-run.log 2>> ../scripts/hahlabs-build-run.err < /dev/null & 
-cd ../angular && nohup ./build-run >> ../scripts/hahlabs-build-run.log 2>> ../scripts/hahlabs-build-run.err < /dev/null &
+cd ../ubuntu && ./build-run >> ../scripts/logs/hahlabs-build-run.log 2>> ../scripts/logs/hahlabs-build-run.err < /dev/null
+cd ../mysql && nohup ./build-run >> ../scripts/logs/hahlabs-build-run.log 2>> ../scripts/logs/hahlabs-build-run.err < /dev/null & 
+cd ../laravel && nohup ./build-run >> ../scripts/logs/hahlabs-build-run.log 2>> ../scripts/logs/hahlabs-build-run.err < /dev/null & 
+cd ../angular && nohup ./build-run >> ../scripts/logs/hahlabs-build-run.log 2>> ../scripts/logs/hahlabs-build-run.err < /dev/null &
 
 jobs
