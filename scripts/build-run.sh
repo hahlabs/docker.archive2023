21f16d822480dd265ae966a6fde0e88577536d3d
Edudater <edudate@edudate.academy>
2024-01-04 12:55:58 -0800

    Disable clearance of caches/images for production builds

--- scripts/build-run.sh
+++ scripts/build-run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #usage ./buid-run <image-tag>  ex/ alpha-0.1.12 or lts-3
-docker rm -f $(docker ps -aq)
-docker rmi -f $(docker images -q)
-docker system prune -af
+# docker rm -f $(docker ps -aq)
+# docker rmi -f $(docker images -q)
+# docker system prune -af
 TAG=alpha-00.05.02
 . .env
 # docker-build source .env and set up parameters
@@ -11,4 +11,4 @@ echo "Building docker container: " $CONTAINER_NAME $IMAGE_TAG
 ../scripts/docker-tag.sh $TAG 
 
 echo  $CONTAINER_NAME $IMAGE_TAG " Built [OK]"
-if [ ! -z ${DEBUG+x} ]; then ./test-image.sh; else echo "skip test image."; fi
+if [ -z ${DEBUG+x} ]; then ./test-image.sh; else echo "skip test image."; fi
